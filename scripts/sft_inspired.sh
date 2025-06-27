6e32b51fbf6349cf2b1a6bc59202de4aa30b7e4a
best99317 <2647107776@qq.com>
2025-06-27 04:08:35 +0000

    Testing SFT Result

---
+++ scripts/sft_inspired.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+export HUGGING_FACE_HUB_TOKEN=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
+export WANDB_API_KEY=f5ab2278d3c710ad96e4b6c662e2dbc002fb5eaf
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 WANDB__SERVICE_WAIT=300 torchrun --master_port=56800 --nnodes=1 --nproc_per_node=8 -m finetuning.sft \
+    --dataset_repo datasets/inspired/multiturn_form/train.csv \
+    --output_dir outputs/sft/inspired/test \
+    --model_name meta-llama/Llama-3.2-1B-Instruct \
+    --per_device_train_batch_size 3 \
+    --per_device_eval_batch_size 3 \
+    --gradient_accumulation_steps 4 \
+    --num_train_epochs 15 \
+    --learning_rate 2e-5 \
+    --eval_steps 1 \
+    --logging_steps 1 \
+    --wandb_entity best99317-purdue-university \
+    --wandb_project Multiturn_RL \
+    --use_lora  --use_4bit
